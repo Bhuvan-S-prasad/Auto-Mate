502cdb36252720451218d7123f7d5c573d7d3ede
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2026-03-22 10:49:02 +0530

    feat(landing-page): add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,18 @@
 
 import { useRef } from "react";
 import Navigation from "@/components/Navigation";
-
+import Hero from "@/components/landing-page/Hero";
 
 export default function Home() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
-
-
   return (
     <div
-      className="min-h-screen bg-background text-foreground selection:bg-primary/30 font-sans overflow-x-hidden grain-overlay"
+      className="min-h-screen bg-background text-foreground selection:bg-primary/30 font-sans grain-overlay"
       ref={scrollRef}
     >
       <Navigation />
-
+      <Hero />
     </div>
   );
 }
